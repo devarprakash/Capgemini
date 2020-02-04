ec3c84e48a3893f5508e0329a3c7c64fd00554ba
devarprakash <60639813+devarprakash@users.noreply.github.com>
2020-02-04 15:30:44 +0530

    Add files via upload

---
+++ junit-demo/src/main/java/Employee.java
@@ -0,0 +1,29 @@
+
+public class Employee {
+
+	
+	private int id;
+	public Employee(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	private String name;
+	
+}
